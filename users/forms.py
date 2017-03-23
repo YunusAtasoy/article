2526c5bb741a9c235166bc124e7aebc1389b2d9e
durucanaykut <durucanaykut@gmail.com>
2017-03-23 16:15:29 +0300

    Kullanıcı kaydındaki hatalar giderildi ve şifre doğrulama eklendi.

--- users/forms.py
+++ users/forms.py
@@ -57,14 +57,33 @@ class UserRegisterForm(forms.ModelForm):
     email = forms.EmailField(label=_('E-Mail Address'))
     first_name = forms.CharField(label=_('First Name'), max_length=100)
     last_name = forms.CharField(label=_('Last Name'), max_length=100)
-    password = forms.CharField(label=_('Password'), widget=forms.PasswordInput)
+    error_messages = {
+        'password_mismatch': _("The two password fields didn't match."),
+    }
+    password1 = forms.CharField(label=_("Password"),
+        widget=forms.PasswordInput)
+    password2 = forms.CharField(label=_("Password confirmation"),
+        widget=forms.PasswordInput,
+        help_text=_("Enter the same password as above, for verification."))
 
     class Meta:
         model = User
-        fields = ('user_types', 'email', 'first_name', 'last_name', 'password')
+        fields = ('user_types', 'email', 'first_name',
+                  'last_name', 'password1', 'password2')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'],
+                code='password_mismatch',
+            )
+        return password2
 
     def save(self, commit=True):
         register = super(UserRegisterForm, self).save(commit=False)
+        register.set_password(self.cleaned_data["password1"])
 
         if commit:
             try:
@@ -73,8 +92,9 @@ class UserRegisterForm(forms.ModelForm):
                     email=self.cleaned_data.get('email'),
                     first_name=self.cleaned_data.get('first_name'),
                     last_name=self.cleaned_data.get('last_name'),
-                    password=self.cleaned_data.get('password'),
+                    password=self.cleaned_data.get('password1'),
                 )
+                register.set_password(password)
                 register.save()
             except:
                 register = None
